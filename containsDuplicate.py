7576aa29e8add54f37b43c4950016c6c7d9d6c67
Deep Naik <deepnaik1809@gmail.com>
2025-07-27 17:11:07 +0530

    Solved Contains Duplicate on LeetCode with new Approach

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,16 +1,20 @@
 class Solution:
     def containsDuplicate(self, nums):
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
-
-
-nums = [1, 2, 3, 1]
+        return len(nums) != len(set(nums))
+nums = [1, 2, 3, 4]
 s = Solution()
 print(s.containsDuplicate(nums))
+
+
+# Most optimal and fastest soltion
+   # def containsDuplicate(self, nums):
+    #     seen = set()
+    #     for num in nums:
+    #         if num in seen:
+    #             return True
+    #         seen.add(num)
+    #     return False
+
 
 
 # Ran the following code, worked but it exceed the time limit at the 65th Testcase out of 77 in LeetCode
